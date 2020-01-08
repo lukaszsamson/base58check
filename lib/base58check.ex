09af16db3eb117bc33f2199f705a4dfb6741fd89
Lukasz Samson <lukaszsamson@gmail.com>
2020-01-08 11:10:30 +0100

    add specs, run formatter

--- lib/base58check.ex
+++ lib/base58check.ex
@@ -1,5 +1,4 @@
 defmodule Base58Check do
-
   b58_alphabet = Enum.with_index('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz')
 
   for {encoding, value} <- b58_alphabet do
@@ -9,64 +8,84 @@ defmodule Base58Check do
 
   defp do_decode58(c), do: raise(ArgumentError, "illegal character #{c}")
 
+  @spec encode58(binary | non_neg_integer) :: binary
   def encode58(data) do
     encoded_zeroes = convert_leading_zeroes(data, [])
     integer = if is_binary(data), do: :binary.decode_unsigned(data), else: data
     encode58(integer, [], encoded_zeroes)
   end
-  defp encode58(0, acc, encoded_zeroes), do: to_string([encoded_zeroes|acc])
+
+  defp encode58(0, acc, encoded_zeroes), do: to_string([encoded_zeroes | acc])
+
   defp encode58(integer, acc, encoded_zeroes) do
     encode58(div(integer, 58), [do_encode58(rem(integer, 58)) | acc], encoded_zeroes)
   end
+
   defp convert_leading_zeroes(<<0>> <> data, encoded_zeroes) do
-    encoded_zeroes = ['1'|encoded_zeroes]
+    encoded_zeroes = ['1' | encoded_zeroes]
     convert_leading_zeroes(data, encoded_zeroes)
   end
+
   defp convert_leading_zeroes(_data, encoded_zeroes), do: encoded_zeroes
 
+  @spec decode58(binary) :: non_neg_integer
   def decode58(code) when is_binary(code) do
     decode58(to_charlist(code), 0)
   end
+
   def decode58(_code), do: raise(ArgumentError, "expects base58-encoded binary")
+
   defp decode58([], acc), do: acc
-  defp decode58([c|code], acc) do
-    decode58(code, (acc * 58) + do_decode58(c))
+
+  defp decode58([c | code], acc) do
+    decode58(code, acc * 58 + do_decode58(c))
   end
 
+  @spec encode58check(binary | non_neg_integer, binary | non_neg_integer) :: binary
   def encode58check(prefix, data) when is_binary(prefix) and is_binary(data) do
-    data = case Base.decode16(String.upcase(data)) do
-        {:ok, bin}  ->  bin
-        :error      ->  data
+    data =
+      case Base.decode16(String.upcase(data)) do
+        {:ok, bin} -> bin
+        :error -> data
       end
+
     versioned_data = prefix <> data
     checksum = generate_checksum(versioned_data)
     encode58(versioned_data <> checksum)
   end
+
   def encode58check(prefix, data) do
     prefix = if is_integer(prefix), do: :binary.encode_unsigned(prefix), else: prefix
     data = if is_integer(data), do: :binary.encode_unsigned(data), else: data
     encode58check(prefix, data)
   end
 
+  @spec decode58check(binary, pos_integer) :: {<<_::8>>, binary}
   def decode58check(code, length \\ 25) do
     decoded_bin = decode58(code) |> :binary.encode_unsigned()
     size = byte_size(decoded_bin)
     checksum_size = 4
+
     if size < checksum_size do
-      raise ArgumentError, "address of size #{size} is too short, expected at least #{checksum_size}"
+      raise ArgumentError,
+            "address of size #{size} is too short, expected at least #{checksum_size}"
     end
+
     if size > length do
       raise ArgumentError, "address of size #{size} is too long, expected #{length}"
     end
+
     payload_size = length - 5
 
-    padding = if size < length do
-      for _ <- 1..(length - byte_size(decoded_bin)), into: <<>>, do: <<0>>
-    else
-      <<>>
-    end
+    padding =
+      if size < length do
+        for _ <- 1..(length - byte_size(decoded_bin)), into: <<>>, do: <<0>>
+      else
+        <<>>
+      end
 
-    <<prefix::binary-size(1), payload::binary-size(payload_size), checksum::binary-size(checksum_size)>> = padding <> decoded_bin
+    <<prefix::binary-size(1), payload::binary-size(payload_size),
+      checksum::binary-size(checksum_size)>> = padding <> decoded_bin
 
     if generate_checksum(prefix <> payload) == checksum do
       {prefix, payload}
--- mix.exs
+++ mix.exs
@@ -2,17 +2,28 @@ defmodule Base58Check.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :base58check,
-     version: "0.1.0",
-     elixir: "~> 1.5",
-     deps: deps(),
-     package: package(),
-     name: "Base58Check",
-     source_url: "https://github.com/lukaszsamson/base58check",
-     homepage_url: "https://github.com/lukaszsamson/base58check",
-     description: """
-     Elixir implementation of Base58Check encoding meant for Bitcoin
-     """]
+    [
+      app: :base58check,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      deps: deps(),
+      package: package(),
+      name: "Base58Check",
+      source_url: "https://github.com/lukaszsamson/base58check",
+      homepage_url: "https://github.com/lukaszsamson/base58check",
+      description: """
+      Elixir implementation of Base58Check encoding meant for Bitcoin
+      """,
+      dialyzer: [
+        flags: [
+          :unmatched_returns,
+          :unknown,
+          :error_handling,
+          :race_conditions,
+          :underspecs
+        ]
+      ]
+    ]
   end
 
   def application do
@@ -20,12 +31,14 @@ defmodule Base58Check.Mixfile do
   end
 
   defp package do
-    [contributors: ["Gabriel Jaldon", "Łukasz Samson"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/lukaszsamson/base58check"}]
+    [
+      contributors: ["Gabriel Jaldon", "Łukasz Samson"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/lukaszsamson/base58check"}
+    ]
   end
 
   defp deps do
-    []
+    [{:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}]
   end
 end
